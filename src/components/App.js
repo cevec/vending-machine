9f31a885217a37a7c12d3263231c116c7441c620
Carlos Velasquez <cevelasquezc@gmail.com>
2021-07-07 11:41:43 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="ui container">
+      <div className="ui basic segment">
+        <h2 className="ui header">Vending machine</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
