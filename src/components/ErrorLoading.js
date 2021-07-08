9a7526b61c925fc36defcce1b22dd64d13fd6855
Carlos Velasquez <cevelasquezc@gmail.com>
2021-07-08 01:51:41 -0600

    add functionality to download products in case of error

--- src/components/ErrorLoading.js
+++ src/components/ErrorLoading.js
@@ -1,11 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { fetchProducts } from './products/productsSlice';
 const ErrorLoading = () => {
+    const dispatch = useDispatch();
+
+    const onClick = () => {
+        dispatch(fetchProducts());
+    }
     return (
         <div className="ui placeholder segment">
             <div className="ui icon header">
                 <i className="warning icon" />
                 Error loading products. Please try again.
             </div>
-            <button className="ui primary button">Load products</button>
+            <button onClick={onClick} className="ui primary button">Load products</button>
         </div>
     );
 };
--- src/components/products/productsSlice.js
+++ src/components/products/productsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createSelector } from '@reduxjs/toolkit';
 
-//import productsApi from '../../apis/productsApi';
-import tempProductsApi from '../../apis/tempProductsApi';
+import productsApi from '../../apis/productsApi';
+//import tempProductsApi from '../../apis/tempProductsApi';
 
 const initialState = {
     status: 'idle',
@@ -41,23 +41,9 @@ export const fetchProducts = () => async (dispatch) => {
     dispatch(productsLoading());
     try {
         //todo: fix issue to download products from api
-        //const response = await productsApi.get('/products');
-        /*const response = await fetch('https://vending-machine-test.vercel.app/api/products', {
-            mode: 'no-cors',
-            headers: {
-                //'Content-Type': 'application/json'
-                 'Content-Type': 'application/x-www-form-urlencoded',
-              }
-        });*/
-        //console.log(response);
-        //const json =  await response.json();
-        //console.log(json);
-        //dispatch(productsLoaded(response.data.data));
-        //dispatch(productsLoaded(json.data));
-        dispatch(productsLoaded(tempProductsApi.data));
-        
+        const response = await productsApi.get('/products');
+        dispatch(productsLoaded(response.data.data));        
     } catch (error) {
-        console.log(error);
         dispatch(productsLoadError());
     }  
 };
